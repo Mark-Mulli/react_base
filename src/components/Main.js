39cad6d2833c48a58c1556c9a74ce974c947e3dd
Mark Mulli <markmulli001@gmail.com>
2024-07-19 08:52:43 +0300

    Project Setup One

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,8 +2,16 @@ import React from "react"
 
 export default function Main() {
     return (
-        <main>
-            <p><b>Main content goes here</b></p>
+        <main className="main_content">
+            <h2>Fun Facts About React</h2>
+
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </main>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react"
+import reactLogo from "./images/reactLogo.png"
 
 export default function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav className = "navbar">
+            <div className = "nav-left">
+                <img src = {reactLogo} alt = "React Logo" height = "29px" width = "29px"></img>
+                <h1 className = "head1">ReactFacts</h1>
+            </div>
+            <div className="nav-right">
+                <h3>React Course - Project 1</h3>
+            </div>
+        </nav>
     );
 }
